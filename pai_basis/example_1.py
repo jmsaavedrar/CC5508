dc3f88a9527aa9c692a60fa8708dae9f5b31d1db
Jose M. Saavedra <jose.saavedra@orand.cl>
2019-08-01 11:08:31 -0400

    firs examples

---
+++ pai_basis/example_1.py
@@ -0,0 +1,19 @@
+'''
+Created on Aug 1, 2019
+
+@author: jsaavedr
+Reading an image
+'''
+import pai_io
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    filename = '../images/gray/lion_gray.jpg' 
+    image = pai_io.read_image(filename, as_gray = True)
+    print("shape: {}".format(image.shape))
+    
+    ##showing image
+    plt.imshow(image, cmap = 'gray')
+    plt.title("image")
+    plt.axis('off')
+    plt.show()
